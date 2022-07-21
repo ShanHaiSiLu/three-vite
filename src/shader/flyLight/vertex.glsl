665447e8b4e9f352aa95a04a2a06cdf2a01dffc4
许晶俞 <shanhaisilu@foxmail.com>
2022-07-21 11:25:37 +0800

    完善水云烟雾效果和参数设定

--- src/shader/flyLight/vertex.glsl
+++
@@ -1,9 +0,0 @@
-precision lowp float;
-
-void main() {
-    // position是模型的原始位置，未经过任何变换
-    // modelPosition是模型经过旋转平移之后的坐标位置；
-    vec4 modelPosition = modelMatrix * vec4(position, 1);
-
-    gl_Position = projectionMatrix * viewMatrix * modelPosition;
-}
