a45f7a6dbc921b5465250ccc46bd7f9dd9b65b36
许晶俞 <shanhaisilu@foxmail.com>
2022-08-02 15:11:15 +0800

    完善VR看房相关代码

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from "three";
+import { BoxGeometry } from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // 变量声明 - 基本元素
 let scene, camera, renderer, axesHelper, controls, clock;
@@ -10,12 +11,27 @@ scene = new THREE.Scene();
 // 相机
 let _cameraOption = [45, window.innerWidth / window.innerHeight, 1, 1000];
 camera = new THREE.PerspectiveCamera(..._cameraOption);
-camera.position.set(10, 10, 65);
+camera.position.set(0, 0, 1);
 camera.lookAt(scene.position);
 
 // 辅助坐标
 axesHelper = new THREE.AxesHelper(25);
 scene.add(axesHelper);
+
+const textureLoader = new THREE.TextureLoader();
+
+// 按照顺序排列图片并加载为材质
+let imgArr = ["4_b", "4_f", "4_l", "4_r", "4_u", "4_d"];
+imgArr = imgArr.map(
+  (i) =>
+    new THREE.MeshBasicMaterial({
+      map: textureLoader.load(`./assets/living/${i}.jpg`),
+      side: THREE.DoubleSide
+    })
+);
+
+const cube = new THREE.Mesh(new THREE.BoxGeometry(10, 10, 10), imgArr);
+scene.add(cube);
 
 // 渲染器
 renderer = new THREE.WebGLRenderer();
